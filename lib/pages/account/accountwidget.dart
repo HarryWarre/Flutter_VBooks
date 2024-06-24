dd2dc8b84ce8da52acbe4d413f53e2a45871e95c
HungLew111 <hungleuu1509@gmail.com>
2024-06-24 20:40:55 +0700

    don hang user admin

---
+++ lib/pages/account/accountwidget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:vbooks_source/pages/admin/orderadminpage.dart';
+import 'package:vbooks_source/pages/order/ordermainpage.dart';
+
+class AccountWidget extends StatelessWidget {
+  const AccountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => OrderMainPage(),
+                      ),
+                    );
+                  },
+                  child: Text('Lịch sử mua hàng'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => OrderAdminPage(),
+                      ),
+                    );
+                  },
+                  child: Text('Đơn hàng admin'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.red),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
