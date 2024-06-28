f62176d44dbc5e1d7719f0090d2bfdc0fc8728a7
hoangviet111203 <hoangviet111203@gmail.com>
2024-06-28 23:24:12 +0700

    Button component for administration

--- admin_vbooks/lib/main.dart
+++ admin_vbooks/lib/main.dart
@@ -1,20 +1,29 @@
+import 'package:admin_vbooks/mainpage.dart';
 import 'package:flutter/material.dart';
+// Include the Google Fonts package to provide more text format options
+// https://pub.dev/packages/google_fonts
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    const appName = 'Custom Themes';
+
+    return MaterialApp(
+        title: appName,
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromRGBO(21, 139, 125, 1),
+              // TRY THIS: Change to "Brightness.light"
+              //           and see that all colors change
+              //           to better contrast a light background.
+            ).copyWith(background: Colors.white)),
+        home: const MainPage());
   }
 }
