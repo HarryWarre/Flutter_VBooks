b9dbf20336e73e59e040450cdbf0c94ffe993141
PhamDuc1105 <pduc23032003@gmail.com>
2024-07-02 11:06:15 +0700

    update user page

--- vbooks_source/lib/pages/order/checkyourorder.dart
+++ vbooks_source/lib/pages/order/checkyourorder.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vbooks_source/pages/order/successful_purchase.dart';
+import 'package:vbooks_source/pages/order/successfulpurchase.dart';
 
 void main() {
   runApp(const MaterialApp(
