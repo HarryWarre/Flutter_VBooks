281d6c2d1d94b2753186c589d9b0561b40e132fc
TDAT0105 <ngotrungdat123@gmail.com>
2024-07-18 23:57:36 +0700

    Added expires for resetaccount schema

--- api/controller/collection/changepasswordController.js
+++ api/controller/collection/changepasswordController.js
@@ -1,71 +1,71 @@
-const Account = require('../../models/account')
-const ChangePassword = require('../../models/collection/changepassword')
-const nodemailer = require('nodemailer')
-const bcrypt = require('bcrypt')
-const doten = require('dotenv')
+const Account = require('../../models/account');
+const ChangePassword = require('../../models/collection/changepassword');
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
 
-doten.config()
+dotenv.config();
 
 exports.forgotPassword = async (req, res) => {
-    try{
-        const account = await Account.findOne({email: req.body.email})
-        if(!account) return res.status(404).json({message: 'Email không tồn tại', success: false})
+    try {
+        const account = await Account.findOne({ email: req.body.email });
+        if (!account) return res.status(404).json({ message: 'Email không tồn tại', success: false });
 
-        const passwordReset = new ChangePassword({email: req.body.email})
-        passwordReset.generateOTP()
-        await passwordReset.save()
+        const passwordReset = new ChangePassword({ email: req.body.email });
+        passwordReset.generateOTP();
+        await passwordReset.save();
 
         const transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
                 user: process.env.AUTH_USER,
                 pass: process.env.AUTH_PASSWORD,
-            }   
-        })
+            }
+        });
 
         const mailOptions = {
             to: account.email,
             from: 'passwordreset@yourdomain.com',
-            subject: 'Password Reset OTP',
-            text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
-            Your OTP for password reset is ${passwordReset.otp}. This OTP is valid for one hour.\n\n
-            If you did not request this, please ignore this email and your password will remain unchanged.\n`
+            subject: 'Đổi mật khẩu',
+            text: `Bạn nhận được mail này là bởi vì bạn hoặc một ai đó đã đề nghị đổi mật khẩu 
+            \nMã OTP của bạn là ${passwordReset.otp}. Mã này sẽ có hiệu lực trong 1 giờ. 
+            \nNếu bạn không gửi yêu cầu đổi mật khẩu, xin hãy đừng quan tâm đến mail này và mật khẩu sẽ không bị đổi`
         };
 
         transporter.sendMail(mailOptions, (err) => {
-            if (err) return res.status(500).send('Error sending email');
-            res.status(200).send('An email has been sent to ' + account.email + ' with the OTP.');
+            if (err) return res.status(500).json('Error sending email');
+            res.status(200).json('An email has been sent to ' + account.email + ' with the OTP.');
         });
 
-    }catch(e){
-        res.status(500).send(e.message);
+    } catch (e) {
+        res.status(500).json(e.message);
     }
 }
 
 exports.resetPassword = async (req, res) => {
     try {
-        const { otp, oldPassword, newPassword} = req.body
+        const { otp, oldPassword, newPassword } = req.body;
 
         const passwordReset = await ChangePassword.findOne({
             otp,
             otpExpires: { $gt: Date.now() }
-        })
+        });
 
-        if(!passwordReset) return res.status(400).send('Mã OTP đã hết hạn')
+        if (!passwordReset) return res.status(400).send('Mã OTP đã hết hạn');
 
-        const account = await Account.findOne({ email: passwordReset.email })
-        if (!account) return res.status(404).send('No account with that email found.')
-    
-        const isMatch = await bcrypt.compare(oldPassword, account.password)
-        if (!isMatch) return res.status(400).send('Old password is incorrect.')
+        const account = await Account.findOne({ email: passwordReset.email });
+        if (!account) return res.status(404).send('No account with that email found.');
 
-        account.password = newPassword
-        await account.save()
+        const isMatch = await bcrypt.compare(oldPassword, account.password);
+        if (!isMatch) return res.status(400).send('Old password is incorrect.');
 
-        await ChangePassword.deleteOne({otp});
-        res.status(200).json('Đổi mật khẩu thành công')
-        
+        account.password = newPassword;
+        await account.save();
+
+        await ChangePassword.deleteOne({ otp });
+        res.status(200).json('Đổi mật khẩu thành công');
+
     } catch (e) {
-        res.status(500).json(e.message)
+        res.status(500).json(e.message);
     }
 }
--- api/models/collection/changepassword.js
+++ api/models/collection/changepassword.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-
-const ChangePassword = new mongoose.Schema({
-    email: { type: String, required: true, unique: true},
+const ChangePasswordSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
     otp: { type: String, required: true },
-    otpExpires: { type: Date, required: true }
-})
+    otpExpires: { type: Date, default: Date.now, expires: 3600 } // hạn sử dụng 1h
+});
 
-ChangePassword.methods.generateOTP = function() {
+ChangePasswordSchema.methods.generateOTP = function() {
     this.otp = Math.floor(10000 + Math.random() * 90000).toString(); // 5 digit OTP
-    this.otpExpires = Date.now() + 3600000; // 1 hour 
+    this.otpExpires = Date.now() + 3600000; // 1 tiếng
 };
 
-module.exports = mongoose.model('ChangePassword', ChangePassword)
+module.exports = mongoose.model('ChangePassword', ChangePasswordSchema);
